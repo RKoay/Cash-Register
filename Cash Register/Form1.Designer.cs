7990c770f59096dce1718eac28caa77d3f778165
Rie. K <riekoay582@ed.amdsb.ca>
2017-10-11 12:01:09 -0400

    Add project files.

---
+++ Cash Register/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Cash_Register
+{
+    partial class mainDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayO = new System.Windows.Forms.Label();
+            this.displayTh = new System.Windows.Forms.Label();
+            this.displayT = new System.Windows.Forms.Label();
+            this.textboxO = new System.Windows.Forms.MaskedTextBox();
+            this.textboxTh = new System.Windows.Forms.MaskedTextBox();
+            this.textboxT = new System.Windows.Forms.MaskedTextBox();
+            this.clickO = new System.Windows.Forms.Button();
+            this.displayF = new System.Windows.Forms.Label();
+            this.displayFi = new System.Windows.Forms.Label();
+            this.displayS = new System.Windows.Forms.Label();
+            this.decorationO = new System.Windows.Forms.Label();
+            this.displaySe = new System.Windows.Forms.Label();
+            this.displayE = new System.Windows.Forms.Label();
+            this.textboxSe = new System.Windows.Forms.MaskedTextBox();
+            this.decorationT = new System.Windows.Forms.Label();
+            this.clickT = new System.Windows.Forms.Button();
+            this.clickTh = new System.Windows.Forms.Button();
+            this.clickF = new System.Windows.Forms.Button();
+            this.blankDisplayO = new System.Windows.Forms.Label();
+            this.blankDisplayT = new System.Windows.Forms.Label();
+            this.blankDisplayTh = new System.Windows.Forms.Label();
+            this.blankDisplayF = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayO
+            // 
+            this.displayO.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayO.Location = new System.Drawing.Point(30, 30);
+            this.displayO.Name = "displayO";
+            this.displayO.Size = new System.Drawing.Size(170, 23);
+            this.displayO.TabIndex = 0;
+            this.displayO.Text = "Number  Of  Burger(s)";
+            // 
+            // displayTh
+            // 
+            this.displayTh.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayTh.Location = new System.Drawing.Point(30, 106);
+            this.displayTh.Name = "displayTh";
+            this.displayTh.Size = new System.Drawing.Size(170, 23);
+            this.displayTh.TabIndex = 1;
+            this.displayTh.Text = "Number  Of  Drink(s)";
+            // 
+            // displayT
+            // 
+            this.displayT.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayT.Location = new System.Drawing.Point(30, 65);
+            this.displayT.Name = "displayT";
+            this.displayT.Size = new System.Drawing.Size(170, 23);
+            this.displayT.TabIndex = 2;
+            this.displayT.Text = "Number  Of  Fries(s)";
+            // 
+            // textboxO
+            // 
+            this.textboxO.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxO.Location = new System.Drawing.Point(229, 27);
+            this.textboxO.Name = "textboxO";
+            this.textboxO.Size = new System.Drawing.Size(69, 35);
+            this.textboxO.TabIndex = 3;
+            // 
+            // textboxTh
+            // 
+            this.textboxTh.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxTh.Location = new System.Drawing.Point(229, 103);
+            this.textboxTh.Name = "textboxTh";
+            this.textboxTh.Size = new System.Drawing.Size(69, 35);
+            this.textboxTh.TabIndex = 4;
+            // 
+            // textboxT
+            // 
+            this.textboxT.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxT.Location = new System.Drawing.Point(229, 65);
+            this.textboxT.Name = "textboxT";
+            this.textboxT.Size = new System.Drawing.Size(69, 35);
+            this.textboxT.TabIndex = 5;
+            // 
+            // clickO
+            // 
+            this.clickO.BackColor = System.Drawing.Color.MistyRose;
+            this.clickO.Font = new System.Drawing.Font("Tekton Pro Ext", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickO.Location = new System.Drawing.Point(57, 156);
+            this.clickO.Name = "clickO";
+            this.clickO.Size = new System.Drawing.Size(223, 46);
+            this.clickO.TabIndex = 6;
+            this.clickO.Text = "Calculate Total";
+            this.clickO.UseVisualStyleBackColor = false;
+            this.clickO.Click += new System.EventHandler(this.clickO_Click);
+            // 
+            // displayF
+            // 
+            this.displayF.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayF.Location = new System.Drawing.Point(30, 220);
+            this.displayF.Name = "displayF";
+            this.displayF.Size = new System.Drawing.Size(170, 23);
+            this.displayF.TabIndex = 7;
+            this.displayF.Text = "Subtotal";
+            // 
+            // displayFi
+            // 
+            this.displayFi.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayFi.Location = new System.Drawing.Point(30, 260);
+            this.displayFi.Name = "displayFi";
+            this.displayFi.Size = new System.Drawing.Size(170, 23);
+            this.displayFi.TabIndex = 8;
+            this.displayFi.Text = "Tax";
+            // 
+            // displayS
+            // 
+            this.displayS.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayS.Location = new System.Drawing.Point(30, 301);
+            this.displayS.Name = "displayS";
+            this.displayS.Size = new System.Drawing.Size(170, 23);
+            this.displayS.TabIndex = 9;
+            this.displayS.Text = "Grand Total";
+            // 
+            // decorationO
+            // 
+            this.decorationO.BackColor = System.Drawing.Color.Black;
+            this.decorationO.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decorationO.Location = new System.Drawing.Point(-5, 327);
+            this.decorationO.Name = "decorationO";
+            this.decorationO.Size = new System.Drawing.Size(310, 23);
+            this.decorationO.TabIndex = 13;
+            // 
+            // displaySe
+            // 
+            this.displaySe.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displaySe.Location = new System.Drawing.Point(30, 365);
+            this.displaySe.Name = "displaySe";
+            this.displaySe.Size = new System.Drawing.Size(170, 23);
+            this.displaySe.TabIndex = 14;
+            this.displaySe.Text = "Tendered";
+            // 
+            // displayE
+            // 
+            this.displayE.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayE.Location = new System.Drawing.Point(30, 457);
+            this.displayE.Name = "displayE";
+            this.displayE.Size = new System.Drawing.Size(170, 23);
+            this.displayE.TabIndex = 15;
+            this.displayE.Text = "Change";
+            // 
+            // textboxSe
+            // 
+            this.textboxSe.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxSe.Location = new System.Drawing.Point(229, 353);
+            this.textboxSe.Name = "textboxSe";
+            this.textboxSe.Size = new System.Drawing.Size(69, 35);
+            this.textboxSe.TabIndex = 16;
+            // 
+            // decorationT
+            // 
+            this.decorationT.BackColor = System.Drawing.Color.Black;
+            this.decorationT.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decorationT.Location = new System.Drawing.Point(304, 0);
+            this.decorationT.Name = "decorationT";
+            this.decorationT.Size = new System.Drawing.Size(20, 575);
+            this.decorationT.TabIndex = 18;
+            // 
+            // clickT
+            // 
+            this.clickT.BackColor = System.Drawing.Color.MistyRose;
+            this.clickT.Font = new System.Drawing.Font("Tekton Pro Ext", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickT.Location = new System.Drawing.Point(57, 391);
+            this.clickT.Name = "clickT";
+            this.clickT.Size = new System.Drawing.Size(223, 46);
+            this.clickT.TabIndex = 19;
+            this.clickT.Text = "Calculate Change";
+            this.clickT.UseVisualStyleBackColor = false;
+            this.clickT.Click += new System.EventHandler(this.clickT_Click);
+            // 
+            // clickTh
+            // 
+            this.clickTh.BackColor = System.Drawing.Color.MistyRose;
+            this.clickTh.Font = new System.Drawing.Font("Tekton Pro Ext", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickTh.Location = new System.Drawing.Point(57, 499);
+            this.clickTh.Name = "clickTh";
+            this.clickTh.Size = new System.Drawing.Size(223, 46);
+            this.clickTh.TabIndex = 20;
+            this.clickTh.Text = "Print Receipt";
+            this.clickTh.UseVisualStyleBackColor = false;
+            this.clickTh.Click += new System.EventHandler(this.clickTh_Click);
+            // 
+            // clickF
+            // 
+            this.clickF.Font = new System.Drawing.Font("Stencil Std", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickF.Location = new System.Drawing.Point(371, 499);
+            this.clickF.Name = "clickF";
+            this.clickF.Size = new System.Drawing.Size(279, 64);
+            this.clickF.TabIndex = 22;
+            this.clickF.Text = "New Order";
+            this.clickF.UseVisualStyleBackColor = true;
+            // 
+            // blankDisplayO
+            // 
+            this.blankDisplayO.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.blankDisplayO.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blankDisplayO.Location = new System.Drawing.Point(141, 220);
+            this.blankDisplayO.Name = "blankDisplayO";
+            this.blankDisplayO.Size = new System.Drawing.Size(157, 19);
+            this.blankDisplayO.TabIndex = 23;
+            this.blankDisplayO.Text = " ";
+            // 
+            // blankDisplayT
+            // 
+            this.blankDisplayT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.blankDisplayT.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blankDisplayT.Location = new System.Drawing.Point(143, 260);
+            this.blankDisplayT.Name = "blankDisplayT";
+            this.blankDisplayT.Size = new System.Drawing.Size(155, 19);
+            this.blankDisplayT.TabIndex = 24;
+            this.blankDisplayT.Text = " ";
+            // 
+            // blankDisplayTh
+            // 
+            this.blankDisplayTh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.blankDisplayTh.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blankDisplayTh.Location = new System.Drawing.Point(142, 301);
+            this.blankDisplayTh.Name = "blankDisplayTh";
+            this.blankDisplayTh.Size = new System.Drawing.Size(156, 19);
+            this.blankDisplayTh.TabIndex = 25;
+            this.blankDisplayTh.Text = " ";
+            // 
+            // blankDisplayF
+            // 
+            this.blankDisplayF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.blankDisplayF.Font = new System.Drawing.Font("Adobe Caslon Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blankDisplayF.Location = new System.Drawing.Point(229, 457);
+            this.blankDisplayF.Name = "blankDisplayF";
+            this.blankDisplayF.Size = new System.Drawing.Size(69, 19);
+            this.blankDisplayF.TabIndex = 26;
+            this.blankDisplayF.Text = " ";
+            // 
+            // mainDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(690, 575);
+            this.Controls.Add(this.blankDisplayF);
+            this.Controls.Add(this.blankDisplayTh);
+            this.Controls.Add(this.blankDisplayT);
+            this.Controls.Add(this.blankDisplayO);
+            this.Controls.Add(this.clickF);
+            this.Controls.Add(this.clickTh);
+            this.Controls.Add(this.clickT);
+            this.Controls.Add(this.decorationT);
+            this.Controls.Add(this.textboxSe);
+            this.Controls.Add(this.displayE);
+            this.Controls.Add(this.displaySe);
+            this.Controls.Add(this.decorationO);
+            this.Controls.Add(this.displayS);
+            this.Controls.Add(this.displayFi);
+            this.Controls.Add(this.displayF);
+            this.Controls.Add(this.clickO);
+            this.Controls.Add(this.textboxT);
+            this.Controls.Add(this.textboxTh);
+            this.Controls.Add(this.textboxO);
+            this.Controls.Add(this.displayT);
+            this.Controls.Add(this.displayTh);
+            this.Controls.Add(this.displayO);
+            this.Name = "mainDisplay";
+            this.Text = "Great Burger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label displayO;
+        private System.Windows.Forms.Label displayTh;
+        private System.Windows.Forms.Label displayT;
+        private System.Windows.Forms.MaskedTextBox textboxO;
+        private System.Windows.Forms.MaskedTextBox textboxTh;
+        private System.Windows.Forms.MaskedTextBox textboxT;
+        private System.Windows.Forms.Button clickO;
+        private System.Windows.Forms.Label displayF;
+        private System.Windows.Forms.Label displayFi;
+        private System.Windows.Forms.Label displayS;
+        private System.Windows.Forms.Label decorationO;
+        private System.Windows.Forms.Label displaySe;
+        private System.Windows.Forms.Label displayE;
+        private System.Windows.Forms.MaskedTextBox textboxSe;
+        private System.Windows.Forms.Label decorationT;
+        private System.Windows.Forms.Button clickT;
+        private System.Windows.Forms.Button clickTh;
+        private System.Windows.Forms.Button clickF;
+        private System.Windows.Forms.Label blankDisplayO;
+        private System.Windows.Forms.Label blankDisplayT;
+        private System.Windows.Forms.Label blankDisplayTh;
+        private System.Windows.Forms.Label blankDisplayF;
+    }
+}
+
---
+++ Cash Register/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Cash_Register
+{
+    public partial class mainDisplay : Form
+    {
+        public mainDisplay()
+        {
+            InitializeComponent();
+        }
+
+        //global variables
+        const double BURGERCOST = 2.49;
+        const double FRIESCOST = 1.89;
+        const double DRINKSCOST = 0.99;
+        const double TAXRATIO = 0.13;
+
+        int burgerAmount = 0;
+        int friesAmount = 0;
+        int drinksAmount = 0;
+        double givenAmount = 0;
+
+        double subTotal = 0;
+        double taxes = 0;
+        double grandTotal = 0;
+        double change = 0;
+        double totalBurgerCost = 0;
+        double totalFriesCost = 0;
+        double totalDrinksCost = 0;
+
+        private void clickO_Click(object sender, EventArgs e)
+        {
+            burgerAmount = Convert.ToInt16(textboxO.Text);
+            friesAmount = Convert.ToInt16(textboxT.Text);
+            drinksAmount = Convert.ToInt16(textboxTh.Text);
+            totalBurgerCost = burgerAmount * BURGERCOST;
+            totalBurgerCost.ToString("C");
+            totalFriesCost = friesAmount * FRIESCOST;
+            totalFriesCost.ToString("C");
+            totalDrinksCost = drinksAmount * DRINKSCOST;
+            totalDrinksCost.ToString("C");
+            subTotal = totalBurgerCost + totalFriesCost + totalDrinksCost;
+            blankDisplayO.Text = subTotal.ToString("C");
+
+            taxes = TAXRATIO * subTotal;
+            blankDisplayT.Text = taxes.ToString("C");
+
+            grandTotal = taxes + subTotal;
+            grandTotal.ToString("C");
+            blankDisplayTh.Text = grandTotal.ToString("C");
+        }
+
+        private void clickT_Click(object sender, EventArgs e)
+        {
+            givenAmount = Convert.ToInt16(textboxSe.Text);
+            givenAmount.ToString("0.00");
+            change = givenAmount - grandTotal;
+            change.ToString("C");
+            blankDisplayF.Text = change.ToString("C");
+
+        }
+
+        private void clickTh_Click(object sender, EventArgs e)
+        {
+
+            string lineOne = "Great Burger Company";
+            string lineTwo = "Hamburger";
+            string lineThree = "Fries";
+            string lineFour = "Drinks";
+            string lineFive = "Subtotal";
+            string lineSix = "Taxes";
+            string lineSeven = "Grand Total";
+            string lineEight = "Amount Given";
+            string lineNine = "Change";
+            string lineTen = "@";
+            string lineEleven = "each";
+
+            Graphics receiptDisplay = this.CreateGraphics();
+            Font font = new Font("consolas", 12, FontStyle.Underline);
+            SolidBrush colour = new SolidBrush(Color.Black);
+            receiptDisplay.DrawString(lineOne, font, colour, 370, 20);
+            Thread.Sleep(500);
+            font = new Font("consolas", 12);
+            receiptDisplay.DrawString(lineTwo + " x" + burgerAmount + lineTen + " $" + BURGERCOST + lineEleven, font, colour, 350, 40);
+            receiptDisplay.DrawString("$" + totalBurgerCost, font, colour, 600, 40);
+            Thread.Sleep(500);
+            receiptDisplay.DrawString(lineThree + " x" + friesAmount + lineTen + " $" + FRIESCOST + lineEleven, font, colour, 350, 60);
+            receiptDisplay.DrawString("$" + totalFriesCost, font, colour, 600, 60);
+            Thread.Sleep(500);
+            receiptDisplay.DrawString(lineFour + " x" + drinksAmount + lineTen + " $" + DRINKSCOST + lineEleven, font, colour, 350, 80);
+            receiptDisplay.DrawString("$" + totalDrinksCost, font, colour, 600, 80);
+            Thread.Sleep(500);
+            receiptDisplay.DrawString(lineFive, font, colour, 350, 100);
+            receiptDisplay.DrawString("$" + subTotal, font, colour, 600, 100);
+            Thread.Sleep(500);
+            receiptDisplay.DrawString(lineSix, font, colour, 350, 120);
+            receiptDisplay.DrawString("$" + taxes, font, colour, 600, 120);
+            Thread.Sleep(500);
+            receiptDisplay.DrawString(lineSeven, font, colour, 350, 180);
+            receiptDisplay.DrawString("$" + grandTotal, font, colour, 600, 180);
+            Thread.Sleep(500);
+            receiptDisplay.DrawString(lineEight, font, colour, 350, 200);
+            receiptDisplay.DrawString("$" + givenAmount, font, colour, 600, 200);
+            Thread.Sleep(500);
+            receiptDisplay.DrawString(lineNine, font, colour, 350, 240);
+            receiptDisplay.DrawString("$" + change, font, colour, 600, 240);
+
+
+        }
+    }
+}
